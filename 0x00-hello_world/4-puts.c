e8796f1b8ef20baacd7f9031b16b1af9ebccae66
fitsum-design <100583996+fitsum-design@users.noreply.github.com>
2022-03-12 22:07:04 +0300

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints text using puts()
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
