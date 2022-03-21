0902f95e4869ccac6e78ccd8a5170fa17e7ecefb
fitsum-design <fitsumatnafu02@gmail.com>
2022-03-21 11:47:36 +0300

    summit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,20 @@
 #include "main.h"
-
 /**
-* _isupper - Checks if c is uppercase character
-* @c: Character to be checked
-*
-* Return: 1 if c is uppercase, 0 otherwise
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
 */
 int _isupper(int c)
 {
+
+
 	if (c >= 'A' && c <= 'Z')
-		return (1);
+	{
+	return (1);
+	}
 	else
-		return (0);
+{
+	return (0);
+}
+
 }
